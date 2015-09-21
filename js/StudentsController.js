164c43a5930373c7a7f87f66b9c2a12f97d51450
Kelli Margaret & Austin Blanchard <student@epicodus.com>
2015-09-21 11:14:10 -0700

    Add initial files.

---
+++ js/StudentsController.js
@@ -0,0 +1,8 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+    $scope.students = [
+        { name: "Austin Blanchard" },
+        { name: "Beyonce Knowles" },
+        { name: "Shawn Carter "},
+        { name: "Snoop Dog"}
+    ]
+});
